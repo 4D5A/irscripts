d504fd84141fd777040345c0b53d4f4c53105dff
4D5A <4D5A@users.noreply.github.com>
2019-06-03 14:50:09 -0400

    Update Get-O365Logs.ps1

--- powershell/O365/Get-O365Logs.ps1
+++ powershell/O365/Get-O365Logs.ps1
@@ -1,16 +1,26 @@
-$StartDate = (Get-Date).Adddays(-14)
-$EndDate = (Get-Date)
-$UPN = Read-Host "Enter the User Principal Name of the account for which you want to obtain logs"
+$AdminAuditLogEnabled = Get-AdminAuditLogConfig | Select -ExpandProperty UnifiedAuditLogIngestionEnabled
 $Dehydrated = Get-OrganizationConfig | Select -ExpandProperty IsDehydrated
+$EndDate = (Get-Date)
+$StartDate = (Get-Date).Adddays(-14)
+$UPN = Read-Host "Enter the User Principal Name of the account for which you want to obtain logs"
+
 If ($Dehydrated -eq $True) {
-  Write-Host "It is Dehydrated."
+  Write-Host "Organization Customization is disabled in this tenant. Enabling Organization Customization in this tenant."
   Enable-OrganizationCustomization
-  Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+  Write-Host "Enabling the Admin Audit Log."
+  Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $True
 }
 If ($Dehydrated -eq $False) {
-  Write-Host "It is not Dehydrated."
-  Get-MessageTrace -StartDate $StartDate -EndDate $EndDate -RecipientAddress $UPN | Select MessageId, Organization, Received, SenderAddress, RecipientAddress, Subject, Status, FromIP, ToIP, PSComputerName, RunspaceId, MessageTraceId | Export-Csv "$($env:USERPROFILE)\Desktop\Get-MessageTrace Report $UPN.csv" –NoTypeInformation -Encoding UTF8
-  Search-UnifiedAuditLog -StartDate $StartDate -EndDate $EndDate | Select-Object -ExpandProperty AuditData | ConvertFrom-Json | Select CreationTime, Operation, ResultStatus, ClientIP, UserId, RequestType, ResultStatusDetail, ActorIpAddress | Where {($_.ActorIpAddress -ne $null)} | Export-Csv "$($env:USERPROFILE)\Desktop\Search-UnifiedAuditLog Report $UPN.csv" –NoTypeInformation -Encoding UTF8
+  If ($AdminAuditLogEnabled -eq $True) {
+    Write-Host "Running Get-MessageTrace."
+    Get-MessageTrace -StartDate $StartDate -EndDate $EndDate -RecipientAddress $UPN | Select MessageId, Organization, Received, SenderAddress, RecipientAddress, Subject, Status, FromIP, ToIP, PSComputerName, RunspaceId, MessageTraceId | Export-Csv "$($env:USERPROFILE)\Desktop\Get-MessageTrace Report $UPN.csv" –NoTypeInformation -Encoding UTF8
+    Write-Host "Running Search-UnifiedAuditLog."
+    Search-UnifiedAuditLog -StartDate $StartDate -EndDate $EndDate | Select-Object -ExpandProperty AuditData | ConvertFrom-Json | Select CreationTime, Operation, ResultStatus, ClientIP, UserId, RequestType, ResultStatusDetail, ActorIpAddress | Where {($_.ActorIpAddress -ne $null)} | Export-Csv "$($env:USERPROFILE)\Desktop\Search-UnifiedAuditLog Report $UPN.csv" –NoTypeInformation -Encoding UTF8
+  }
+  If ($AdminAuditLogEnabled -eq $False) {
+    Write-Host "Enabling the Admin Audit Log."
+    Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $True
+  }
 }
 $AccountTakeover = Read-Host "Based on the information included in the log files, do you believe this account is compromised? (Yes/No)"
 If ($AccountTakeover -like "y") {
